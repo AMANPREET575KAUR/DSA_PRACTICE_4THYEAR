0de473b240b55682200bf490af7f1d1ad1f08bd7
AMANPREET575KAUR <amanpreet575575@gmail.com>
2026-07-07 19:54:46 +0530

    Maxsubarrya kadane's algo

---
+++ src/MAXSUBARRAY_KADANESALGO.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class MAXSUBARRAY_KADANESALGO {
+    public static int maxsub(int arr[]){
+        int currsum=arr[0];
+        int maxsum=arr[0];
+        for(int i=1;i<arr.length;i++){
+            currsum=Math.max(arr[i],arr[i]+currsum);
+            maxsum=Math.max(maxsum,currsum);
+        }
+        return maxsum;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int ans=maxsub(arr);
+        System.out.println(ans);
+    }
+}
