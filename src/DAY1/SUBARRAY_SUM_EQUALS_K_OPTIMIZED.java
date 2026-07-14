6459c6d48e7590b715e8d5901ea0de8bea3f4172
AMANPREET575KAUR <amanpreet575575@gmail.com>
2026-07-14 17:55:56 +0530

    DAY5 rotten oranges code

--- src/DAY1/SUBARRAY_SUM_EQUALS_K_OPTIMIZED.java
+++ src/DAY1/SUBARRAY_SUM_EQUALS_K_OPTIMIZED.java
@@ -6,33 +6,21 @@ public class SUBARRAY_SUM_EQUALS_K_OPTIMIZED {
 
     public static int subarraySum(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
-
-        // prefix sum 0 occurs once initially
         map.put(0, 1);
-
         int prefixSum = 0;
         int count = 0;
-
         for (int num : nums) {
             prefixSum += num;
-
-            // Check if there is a previous prefix sum = prefixSum - k
             if (map.containsKey(prefixSum - k)) {
                 count += map.get(prefixSum - k);
             }
-
-            // Store/update current prefix sum frequency
             map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
         }
-
         return count;
     }
-
     public static void main(String[] args) {
-
         int[] nums = {1, 2, 3};
         int k = 3;
-
         System.out.println("Number of subarrays: " + subarraySum(nums, k));
     }
 }
